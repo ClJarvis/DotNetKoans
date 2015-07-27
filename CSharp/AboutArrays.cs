20fb0c6fcd90df02578fca432271a14ae2503ae3
Chris_Jarvis <clj@ollure.com>
2015-07-27 00:09:12 -0500

    array koan1

--- CSharp/AboutArrays.cs
+++ CSharp/AboutArrays.cs
@@ -11,13 +11,13 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void CreatingArrays()
         {
-            var empty_array = new object[] { };
-            Assert.Equal(typeof(FillMeIn), empty_array.GetType());
+            int[] empty_array = new int[0] { };
+            Assert.Equal(typeof(int[]), empty_array.GetType());
 
             //Note that you have to explicitly check for subclasses
             Assert.True(typeof(Array).IsAssignableFrom(empty_array.GetType()));
 
-            Assert.Equal(FILL_ME_IN, empty_array.Length);
+            Assert.Equal(0, empty_array.Length);
         }
 
         [Koan(2)]
